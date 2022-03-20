3e66862d99dbd277946ba64d735ab09502face5e
Maarten Hogeveen <maarten@maartens-mbp.lan>
2022-03-20 19:22:29 +0100

    Create simple Auth component

---
+++ src/lib/components/Auth.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+  import { supabase } from "$lib/supabase/client";
+
+  const signIn = async () => {
+    const { user, session, error } = await supabase.auth.signIn({
+      provider: 'google',
+    })
+  }
+
+  const signOut = async () => {
+    const { error } = await supabase.auth.signOut()
+  }
+</script>
+
+<button on:click={signIn}>Sign In</button>
+<button on:click={signOut}>Sign Out</button>
