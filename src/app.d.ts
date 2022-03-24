25c2b4ebc327592ebbb4df27c662193b783eafc1
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-03-24 20:59:14 +0100

    Move env variable interfaces to app.d.ts

--- src/app.d.ts
+++ src/app.d.ts
@@ -8,3 +8,12 @@ declare namespace App {
 	// interface Session {}
 	// interface Stuff {}
 }
+
+interface ImportMetaEnv {
+  readonly VITE_SUPABASE_URL: string;
+  readonly VITE_SUPABASE_ANON_KEY: string;
+}
+
+interface ImportMeta {
+  readonly env: ImportMetaEnv
+}
--- src/global.d.ts
+++
@@ -1,8 +0,0 @@
-interface ImportMetaEnv {
-  readonly VITE_SUPABASE_URL: string;
-  readonly VITE_SUPABASE_ANON_KEY: string;
-}
-
-interface ImportMeta {
-  readonly env: ImportMetaEnv
-}
