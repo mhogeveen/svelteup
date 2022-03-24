5a57b4a7440b29e9d90b836ee48b34c642c16dc0
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-03-24 19:40:25 +0100

    Dynamic Auth buttons

--- src/lib/components/Auth.svelte
+++ src/lib/components/Auth.svelte
@@ -1,16 +1,21 @@
 <script lang="ts">
   import { supabase } from "$lib/supabase/client";
+  import userSession from "$lib/stores/userSession";
 
   const signIn = async () => {
     const { user, session, error } = await supabase.auth.signIn({
       provider: 'google',
     })
   }
-
+  
   const signOut = async () => {
     const { error } = await supabase.auth.signOut()
+    $userSession = null
   }
 </script>
 
-<button on:click={signIn}>Sign In</button>
-<button on:click={signOut}>Sign Out</button>
+{#if $userSession}
+  <button on:click={signOut}>Sign Out</button>
+{:else}
+  <button on:click={signIn}>Sign In</button>
+{/if}
