b251b0b39fcc846e7468428f59e6737b7a0e6713
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-03-24 20:59:51 +0100

    Add Space component

---
+++ src/lib/components/Space.svelte
@@ -0,0 +1,18 @@
+<script lang="ts">
+  let open: boolean = true;
+
+  const toggleSpaceOpen = (e: MouseEvent | KeyboardEvent) => {
+    console.log(e);
+    if ('code' in e && e.code !== 'KeyQ') {
+      return;
+    }
+    open = !open;
+  }
+</script>
+
+<svelte:window on:keyup={toggleSpaceOpen}/>
+
+<aside class="h-full w-80 bg-slate-300 justify-center items-center" class:flex={open} class:hidden={!open}>
+  <button on:click={toggleSpaceOpen}>x</button>
+  Folders & lists
+</aside>
