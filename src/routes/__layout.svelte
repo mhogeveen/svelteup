7b5331b9aff412262c3f86c8ab9bb32ccdd2860d
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-03-24 21:00:01 +0100

    Add first pass for root layout

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -1,7 +1,19 @@
 <script lang="ts">
-  import Auth from '$lib/components/Auth.svelte';
+  import Spaces from '$lib/components/Spaces.svelte';
+  import Space from '$lib/components/Space.svelte';
   import "../app.css";
 </script>
 
-<Auth />
-<slot />
+<main class="h-screen w-full bg-slate-500 flex">
+  <Spaces />
+
+  <section class="grow flex flex-col">
+    <nav class="h-10 w-full flex justify-center items-center bg-slate-400">Favorites</nav>
+    <div class="flex grow">
+      <Space />
+      <div class="grow flex justify-center items-center">
+        Main
+      </div>
+    </div>
+  </section>
+</main>
