3cb038b9ead902b25093fcd811e7bd4ac062f31f
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-03-24 20:59:46 +0100

    Add Spaces component

---
+++ src/lib/components/Spaces.svelte
@@ -0,0 +1,9 @@
+<script lang="ts">
+  import Auth from '$lib/components/Auth.svelte';
+</script>
+
+
+<aside aria-label="Spaces sidebar" class="h-screen w-80 bg-slate-200 flex flex-col justify-center items-center p-2">
+  <!-- <Auth /> -->
+  Spaces
+</aside>
