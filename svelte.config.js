7fd90faf39512dd9139e499b54544a0255bede92
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-04-14 19:36:11 +0200

    Add potential fix for fontawesome deployment issue

--- svelte.config.js
+++ svelte.config.js
@@ -8,8 +8,13 @@ const config = {
 	preprocess: preprocess(),
 
 	kit: {
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		vite: {
+			ssr: {
+				noExternal: ['@fortawesome/*'],
+			},
+		},
+	},
 };
 
 export default config;
