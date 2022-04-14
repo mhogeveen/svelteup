808b1079478e311915dc0df625a394aad839a137
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-04-14 19:04:35 +0200

    Create ui toggles stores

---
+++ src/lib/stores/uiToggles.ts
@@ -0,0 +1,14 @@
+import {writable} from 'svelte/store';
+
+const createActive = (defaultValue: boolean = true) => {
+  const { subscribe, update } = writable(defaultValue);
+
+  return {
+    subscribe,
+    toggle: () => update(value => !value),
+  }
+}
+
+export const spaceActive = createActive();
+
+export const favoritesActive = createActive(false);
