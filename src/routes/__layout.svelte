01448ac8326ac744504964cfed72aaab39e1f766
Maarten Hogeveen <maarten@Maartens-MacBook-Pro.local>
2022-04-14 20:38:06 +0200

    Add Roboto font links

--- src/routes/__layout.svelte
+++ src/routes/__layout.svelte
@@ -2,11 +2,17 @@
   import Spaces from '$lib/components/Spaces.svelte';
   import Space from '$lib/components/Space.svelte';
   import { hotkey } from '$lib/hooks';
-  import { favoritesActive } from '$lib/stores';
+  import { favoritesActive, darkMode } from '$lib/stores';
   import "../app.css";
 </script>
 
-<main class="h-screen w-full bg-slate-500 flex">
+<svelte:head>
+  <link rel="preconnect" href="https://fonts.googleapis.com" />
+  <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin="anonymous" />
+  <link href="https://fonts.googleapis.com/css2?family=Roboto:ital,wght@0,100;0,300;0,400;0,500;0,700;0,900;1,100;1,300;1,400;1,500;1,700;1,900&display=swap" rel="stylesheet" />
+</svelte:head>
+
+<main class="h-screen w-full bg-slate-500 flex" class:dark={$darkMode}>
   <Spaces />
 
   <section class="grow flex flex-col">
@@ -14,7 +20,7 @@
     <div class="flex grow">
       <Space />
       <div class="grow flex justify-center items-center">
-        Main
+        <slot />
       </div>
     </div>
   </section>
